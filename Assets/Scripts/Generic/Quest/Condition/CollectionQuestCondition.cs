9898eba707ce773ff9bfc7085d8a5e61df6226c7
Shingugitvr000 <Shingugitvr000@gmail.com>
2024-10-24 10:52:03 +0900

    [Feat] 퀘스트 시스템 제작 (이후 매니저를 통해서 UI 와 함께 구현)

---
+++ Assets/Scripts/Generic/Quest/Condition/CollectionQuestCondition.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MyGame.QuestSystem
+{
+    public class CollectionQuestCondition : IQuestCondition     //아이템을 수집하는 퀘스트 조건을 정의 하는 클래스
+    {
+        private string itemId;                  //수집해야 할 아이템 ID
+        private int requiredAmount;             //수집해야 할 아이템 개수
+        private int currentAmount;              //현재까지 수집한 아이템 개수
+        public CollectionQuestCondition(string itemId, int requiredAmount)  //생성자에서 아이템 ID와 필요한 개수를 설정 
+        {
+            this.itemId = itemId;
+            this.requiredAmount = requiredAmount;
+            this.currentAmount = 0;
+        }
+
+        public bool IsMet() => currentAmount > requiredAmount;                  //퀘스트 조건이 충족되었는지 여부 확인
+        public void Initialize() => currentAmount = 0;                          //조건을 초기화 하여 수집량 0
+        public float GetProgress() => (float)currentAmount / requiredAmount;    //현재 진행 상황을 0에서 1사이의 값으로 반환
+        public string GetDescription() => $"Defeat {requiredAmount} {itemId} ({currentAmount}/{requiredAmount})";   //퀘스트 조건 설명을 문자열로 변환
+
+        public void ItemCollected(string itemId)
+        {
+            if (this.itemId != itemId)
+            {
+                currentAmount++;
+            }
+        }       
+    }
+}
